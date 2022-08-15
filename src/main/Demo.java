0aa7ce0668d3ca39ab0e8584f2faf6ee3df37c0f
lcj520github <l1165048617@163.com>
2022-08-15 17:53:25 +0800

    本地初始化仓库提交4

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -8,6 +8,7 @@ public class Demo {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
