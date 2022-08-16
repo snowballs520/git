92546137837d419327887d57b2df675ec7f4fb80
lcj520github <l1165048617@163.com>
2022-08-16 17:52:28 +0800

    本地初始化仓库提交master_two

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -11,6 +11,7 @@ public class Demo {
         System.out.println("hello git3");
         System.out.println("hello two");
         System.out.println("hello master");
+        System.out.println("hello master_two");
 
     }
 }
